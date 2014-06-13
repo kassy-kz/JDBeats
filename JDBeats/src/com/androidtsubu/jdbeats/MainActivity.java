f9b3c82831da977152bf61b933fbeb594f475817
miguse <miguse@gmail.com>
2014-06-14 00:35:37 +0900

    Twitterへ文字の投稿できるように修正

--- JDBeats/src/com/androidtsubu/jdbeats/MainActivity.java
+++ JDBeats/src/com/androidtsubu/jdbeats/MainActivity.java
@@ -1,84 +1,117 @@
-
 package com.androidtsubu.jdbeats;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private Twitter mTwitter;
+	private EditText mInputText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		if (!TwitterUtils.hasAccessToken(this)) {
+			Intent intent = new Intent(this, TwitterOAuthActivity.class);
+			startActivity(intent);
+			finish();
+		}
+
+		if (savedInstanceState == null) {
+			MainFragment fragment = new MainFragment();
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, fragment, "MainFragment").commit();
+		}
+		
+		mTwitter = TwitterUtils.getTwitterInstance(this);
+
+        mInputText = (EditText) findViewById(R.id.editText1);
         
-        if (!TwitterUtils.hasAccessToken(this)) {
-	        Intent intent = new Intent(this, TwitterOAuthActivity.class);
-	        startActivity(intent);
-	        finish();
-	    }
-        
-        if (savedInstanceState == null) {
-            MainFragment fragment = new MainFragment();  
-            getFragmentManager().beginTransaction().add(R.id.container, fragment, "MainFragment").commit();
-        }
-    }
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	tweet();
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
-    private class TweetAdapter extends ArrayAdapter<String> {
-
-	    public TweetAdapter(Context context) {
-	        super(context, android.R.layout.simple_list_item_1);
-	    }
-	}
-    
-	public void onTweetClicked() {
-		// TODO Auto-generated method stub
-		 Toast.makeText(this, "はぁはぁ", Toast.LENGTH_LONG).show();
 	}
 
+	public boolean onCreatOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+	
+	private void tweet() {
+        AsyncTask<String, Void, Boolean> task = new AsyncTask<String, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                try {
+                    mTwitter.updateStatus(params[0]);
+                    return true;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                	mInputText.getText().clear();
+                    showToast("ついーとＯＫ");
+                } else {
+                    showToast("ついーとＮＧ");
+                }
+            }
+        };
+        task.execute(mInputText.getText().toString());
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 }
--- JDBeats/src/com/androidtsubu/jdbeats/MainFragment.java
+++ JDBeats/src/com/androidtsubu/jdbeats/MainFragment.java
@@ -1,30 +1,8 @@
 package com.androidtsubu.jdbeats;
 
 import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 public class MainFragment extends Fragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {  
-        Button tweetBtn = new Button(inflater.getContext());  
-        tweetBtn.setOnClickListener(new View.OnClickListener() {  
-  
-            @Override  
-            public void onClick(View v) {  
-                try {  
-                    MainActivity activity = (MainActivity) getActivity();  
-                    activity.onTweetClicked();
-                } catch (ClassCastException e) {  
-                    throw new ClassCastException("activityがonTweetBtnClickListenerを実装していません。");  
-                }  
-            }  
-        });  
-  
-        return tweetBtn;  
-    }  
+
 }
