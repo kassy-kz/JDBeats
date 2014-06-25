58e2108bddedc30fe4fe4ca2aebf0192e1668d8e
Ikuo Tansho <ikuo.tansho@gmail.com>
2014-06-25 12:39:20 +0900

    Google chart tools（新API）置き換え

--- JDBeats/src/com/androidtsubu/jdbeats/ChartFragment2.java
+++ JDBeats/src/com/androidtsubu/jdbeats/ChartFragment2.java
@@ -1,9 +1,11 @@
 package com.androidtsubu.jdbeats;
 
 import com.androidtsubu.jdbeats.db.JDBeatsDBHelper;
+import com.androidtsubu.jdbeats.event.OnFinishListener;
 import com.androidtsubu.jdbeats.util.JDBeatsHTMLFactory;
 import com.androidtsubu.jdbeats.util.Validator;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
@@ -13,23 +15,91 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class ChartFragment2 extends Fragment {
 
+	/** グラフ表示用WebView　*/
 	private WebView mWebView;
+	/** 表示待ちアニメーション */
+	private ProgressBar mProgressBar;
+	/** チャート描画フラグ */
+	private boolean mIsChartDraw;
+	/** 表描画フラグ */
+	private boolean mIsTableDraw;
+
+	/** 描画終了(成功) */
+	public static final int DRAW_SUCCESS = 1;
 	
+	/** 描画終了(失敗) */
+	public static final int DRAW_FAIL = 0;
+
+	/** 表示待ちアニメーションを停止するためのハンドラ */
+	@SuppressLint("HandlerLeak")
+	private Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch(msg.what) {
+			case 1:
+				mIsChartDraw = true;
+				break;
+			case 2:
+				mIsTableDraw = true;
+				break;
+
+			case -1:	// 失敗
+				mIsChartDraw = false;
+				mIsTableDraw = false;
+				if (mListener != null) {
+					mListener.onFinish(DRAW_FAIL);
+				}
+			}
+
+			// フラグと表の描画が完了したら、プログレス表示を停止し、描画成功を通知する
+			if (mIsChartDraw && mIsTableDraw) {
+				mProgressBar.setVisibility(View.GONE);
+				mProgressBar.setEnabled(false);
+				if (mListener != null) {
+					mListener.onFinish(DRAW_SUCCESS);
+				}
+			} else {
+				mProgressBar.setVisibility(View.VISIBLE);
+			}
+		}
+	};
+
+	
+	/** 描画終了を通知するイベントハンドラ */
+	private OnFinishListener mListener;
+
+	/**
+	 * グラフ描画終了を通知するイベントハンドラ
+	 * @param listener リスナ
+	 */
+	public void setOnFinishListener(OnFinishListener listener) {
+		mListener = listener;
+	}
+	
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_chart2, container, false);
 		mWebView = (WebView) view.findViewById(R.id.webView1);
 		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.addJavascriptInterface(new JavaScriptInterface(), "AndroidEvent");
+		
+		mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar1);
 		return view;
 	}
-
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -43,10 +113,17 @@ public class ChartFragment2 extends Fragment {
 	 * @return キャプチャ画像(グラフ)
 	 */
 	public Bitmap getGraph() {
+		// WebViewが生成されていない場合は画像取得不可
 		if (mWebView == null) {
 			return null;
 		}
-		
+
+		// 読み込み中の場合は画像取得をしない
+		if (mIsChartDraw == false || mIsTableDraw == false) {
+			return null;
+		}
+
+		// WebViewの画像を取得する
 		Bitmap bitmap = Bitmap.createBitmap(
 				mWebView.getWidth(), mWebView.getHeight(), Bitmap.Config.ARGB_8888);
 		Canvas canvas = new Canvas(bitmap);
@@ -59,6 +136,12 @@ public class ChartFragment2 extends Fragment {
 		
 		@Override
 		public Loader<String> onCreateLoader(int id, Bundle args) {
+			mIsChartDraw = false;
+			mIsTableDraw = false;
+
+			mProgressBar.setVisibility(View.VISIBLE);
+			mProgressBar.setEnabled(true);
+
 			return new ChartLoader(getActivity());
 		}
 
@@ -71,6 +154,11 @@ public class ChartFragment2 extends Fragment {
 
 		@Override
 		public void onLoaderReset(Loader<String> loader) {
+			mIsChartDraw = false;
+			mIsChartDraw = false;
+
+			mProgressBar.setVisibility(View.VISIBLE);
+			mProgressBar.setEnabled(true);
 			mWebView.loadData("", "text/html", "utf-8");
 		}
 	};
@@ -78,7 +166,7 @@ public class ChartFragment2 extends Fragment {
 	public static class ChartLoader extends AsyncTaskLoader<String> {
 
 		/** データベース取得数 */
-		private static final int DEF_LIMIT_VALUE = 15;
+		private static final int DEF_LIMIT_VALUE = 10;
 
 		/** アプリケーションコンテキスト */
 		private Context mContext;
@@ -111,7 +199,24 @@ public class ChartFragment2 extends Fragment {
 				return "";
 			}
 
-			return JDBeatsHTMLFactory.createHTML(mContext, cursor);
+			JDBeatsHTMLFactory factory = new JDBeatsHTMLFactory(mContext);
+			return factory.createHTML(cursor);
+		}
+	}
+	
+	public class JavaScriptInterface {
+		@JavascriptInterface
+		public synchronized void readyEvent(int index) {
+			if (mHandler != null) {
+				mHandler.sendEmptyMessage(index);
+			}
+		}
+		
+		@JavascriptInterface
+		public synchronized void errorEvent() {
+			if (mHandler != null) {
+				mHandler.sendEmptyMessage(-1);
+			}
 		}
 	}
 }
--- JDBeats/src/com/androidtsubu/jdbeats/util/JDBeatsHTMLFactory.java
+++ JDBeats/src/com/androidtsubu/jdbeats/util/JDBeatsHTMLFactory.java
@@ -1,104 +1,167 @@
 package com.androidtsubu.jdbeats.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import com.androidtsubu.jdbeats.R;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.database.Cursor;
 
+/**
+ * グラフ描画のHTMLを生成する
+ * @author TAN
+ *
+ */
 public class JDBeatsHTMLFactory {
+
+	/** アプリケーション・コンテキスト */
+	private Context mContext;
+	/** リソースオブジェクト */
+	private Resources mResources;
 
 	/** 残念女王の数値 */
 	private static final double DEF_ZANNEN_VALUE = 291;
 	/** ちゃんりなの数値 */
 	private static final double DEF_JDBOSS_VALUE = 394;
+	/** 日付フォーマット */
+	private static final String DATETIME_FMT = "MM/dd";
 
-	/** Google Chart Tools(HEADER前半) */
-	private static final String HTML_HEAD_FIRST =
-			"<html>" +
-			"<head>" +
-			"<script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>" +
-			"<script type=\"text/javascript\">" +
-			"google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});" +
-			"google.setOnLoadCallback(drawChart);" +
-			"function drawChart() {" +
-			"var data = google.visualization.arrayToDataTable([";
-
-	/** Google Chart Tools(HEADER後半) */
-	private static final String HTML_HEAD_LAST =
-			"]);" +
-			"var options = {" +
-			"title: 'Udezumo Power Performance(UPP)'," +
-			"vAxis: {" +
-			"title: 'UPP'," +
-			"minValue: '0'" +
-			"}," +
-			"legend: {" +
-			"position: 'top'" +
-			"}," +
-			"hAxis: {" +
-			"textPosition: 'none'" +
-			"}" +
-			"};" +
-			"var chart = new google.visualization.LineChart(document.getElementById('chart_div'));" +
-			"chart.draw(data, options);" +
-			"}" +
-			"</script>" +
-			"</head>";
-
-	/** Google Chart Tools(BODY) */
-	private static final String HTML_BODY =
-			"<body>" +
-			"<div id=\"chart_div\" style=\"width: 100%; height: 50%;\">" +
-			"</div>" +
-			"</body>" +
-			"</html>";
 
 	/**
-	 * Google Chart Toolsでグラフを描画するためのHTMLを生成する
+	 * コンストラクタ
 	 * @param context アプリケーション・コンテキスト
+	 */
+	public JDBeatsHTMLFactory(Context context) {
+		mContext = context;
+		mResources = context.getResources();
+	}
+	
+	/**
+	 * Google Chart Toolsでグラフを描画するためのHTMLを生成する
 	 * @param cursor DBカーソル(登録されている測定値)
 	 * @return HTML
 	 */
-	public static String createHTML(Context context, Cursor cursor) {
+	public String createHTML(Cursor cursor) {
+		
+		// HTMLヘッダ部(前半)
+		StringBuilder sb = new StringBuilder(readAssetsFile("index_head_first.txt"));
+
+		// データ生成
+		sb.append(createDataArray(cursor));
+		
+		// グラフタイトル生成
+		sb.append(createChartTitle());
+
+		// HTMLヘッダ部(後半)
+		sb.append(readAssetsFile("index_head_last.txt"));
+		
+		// HTMLボディ部
+		sb.append(readAssetsFile("index_body.txt"));
+
+		// HTMLを生成して、その文字列を返す
+		return sb.toString();
+	}
+
+	/**
+	 * 折れ線グラフのオプションを設定する
+	 * @return オプション文字列
+	 */
+	private String createChartTitle() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("var chart_options = {\n");
+		sb.append("title: '" + mResources.getString(R.string.chart_title) + "',\n");
+		sb.append("vAxis: {\n");
+		sb.append("title: '" + mResources.getString(R.string.chart_v_axis_legend)+ "',\n");
+		sb.append("minValue: '0'\n");
+		sb.append("},\n");
+		sb.append("legend: {\n");
+		sb.append("position: 'top'\n");
+		sb.append("}\n");
+		sb.append("};\n");
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * グラフ・表を表示するためのデータ配列を生成する
+	 * @param cursor データベースカーソルy
+	 * @return データ配列
+	 */
+	private String createDataArray(Cursor cursor) {
 		// 残念女王のUPP数値を設定する
-		String zannenValue = context.getResources().getString(R.string.zannen_value);
+		String zannenValue = mContext.getResources().getString(R.string.zannen_value);
 		zannenValue = Validator.validate(zannenValue, DEF_ZANNEN_VALUE);
 		// ちゃんりなのUPP数値を設定する
-		String jdbossValue = context.getResources().getString(R.string.jdboss_value);
+		String jdbossValue = mContext.getResources().getString(R.string.jdboss_value);
 		jdbossValue = Validator.validate(jdbossValue, DEF_JDBOSS_VALUE);
 
-		// HTMLヘッダ部(前半)
-		StringBuilder sb = new StringBuilder(HTML_HEAD_FIRST);
-		
-		// グラフ数値作成
-		sb.append("['', 'You', 'Zannen', 'Super JD'],");
+		// 日付フォーマット
+		SimpleDateFormat sdf = new SimpleDateFormat(DATETIME_FMT, Locale.JAPAN);
+
+		// データ生成
+		StringBuilder sb = new StringBuilder("var data = new google.visualization.arrayToDataTable([");
+		//タイトル生成
+		sb.append("['" + mResources.getString(R.string.chart_h_axis_legend_1) + "', '" +
+				mResources.getString(R.string.chart_h_axis_legend_2) + "', '" +
+				mResources.getString(R.string.chart_h_axis_legend_3) + "', '" +
+				mResources.getString(R.string.chart_h_axis_legend_4) + "'],");
 
 		// 検索結果の先頭に移動できるか？
-		int index = 1;
 		if (cursor.moveToFirst() == false) {
 			// 移動できない(0件)
-			sb.append("['0', 0, " + zannenValue + ", " + jdbossValue + "],");
-			sb.append("['1', 0, " + zannenValue + ", " + jdbossValue + "],");
+			sb.append("['-', 0, " + zannenValue + ", " + jdbossValue + "],");
+			sb.append("['-', 0, " + zannenValue + ", " + jdbossValue + "],");
 		} else {
 			if (cursor.getCount() == 1) {
 				// 1件のみの場合は、最初の測定値を0にしておく
-				sb.append("['0', 0, " + zannenValue + ", " + jdbossValue + "],");
+				sb.append("['-', 0, " + zannenValue + ", " + jdbossValue + "],");
 			}
 			do {
+				long datetime = cursor.getLong(1);	// 測定日
 				String value = cursor.getString(2);	// 測定値(valule1)の値
 				value = Validator.validate(value, 0.0);
-				sb.append("['" + String.valueOf(index) + "', " + value + ", " + zannenValue + ", " + jdbossValue + "],");
-				index++;
+				sb.append("['" + sdf.format(datetime) + "', " + value + ", " + zannenValue + ", " + jdbossValue + "],");
 			} while (cursor.moveToNext());
 		}
-		
-		// HTMLヘッダ部(後半)
-		sb.append(HTML_HEAD_LAST);
-		
-		// HTMLボディ部
-		sb.append(HTML_BODY);
+		sb.append("]);");
+		return sb.toString();
+	}
 
-		// HTMLを生成して、その文字列を返す
+	/**
+	 * assets/ ディレクトリに格納されているテキストファイルを読み込み、その内容を返す
+	 * @param fileName 読み込むテキストファイル
+	 * @return テキストファイルの内容
+	 */
+	private String readAssetsFile(String fileName) {
+		AssetManager as = mContext.getResources().getAssets();
+		InputStream is = null;
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			try {
+				is = as.open(fileName);
+				br = new BufferedReader(new InputStreamReader(is));
+				String str;
+				while((str = br.readLine()) != null) {
+					sb.append(str + "\n");
+				}
+			} finally {
+				if (br != null) {
+					br.close();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return sb.toString();
 	}
 }
